a6f10b0713d343ad91e3fd280e1132d34ea25f4f
Tursynkhan <t_karimkhanov@kbtu.kz>
2024-04-24 20:13:56 +0500

    adapter + search fr

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,9 +27,6 @@ android {
         }
     }
 
-    buildFeatures {
-        viewBinding = true
-    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +34,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -46,6 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
