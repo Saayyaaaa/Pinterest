4498b6f998e4311e2803613870ab2759b3a2bf60
Saya <s_amangeldinova@kbtu.kz>
2024-05-16 21:12:53 +0500

    get username from login activity

--- app/src/main/java/com/example/pinterest/SettingActivity.kt
+++ app/src/main/java/com/example/pinterest/SettingActivity.kt
@@ -2,6 +2,7 @@ package com.example.pinterest
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +15,12 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setting)
 
         val username = intent.getStringExtra("USERNAME") ?: "Unknown User"
-
         val usernameTextView = findViewById<TextView>(R.id.username)
         usernameTextView.text = username
+
+        val backButton = findViewById<ImageButton>(R.id.go_back)
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
